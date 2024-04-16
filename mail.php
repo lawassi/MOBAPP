bebe03f53e385397bf794885e1d8cd190eeb0880
lawassi <lawassi@yahoo.com>
2024-04-16 13:33:50 +0200

    Add files via upload

--- mail.php
+++ mail.php
@@ -19,32 +19,7 @@ $recipient = "lawassi@yahoo.com";
 mail($recipient, $subject, $message, $mailheader)
 or die("Error");
 
-echo'
->
-    <!DOCTYPE html>
-    <html lang="fr">
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Formulaire Animer</title>
-            <link rel="stylesheet" href="style_e.css">
-        </head>
-        <body>
-            <div class="container">
-                <div class="form">
-                    <form>
-                        <h2>Envoyer avec succes; Merci a vous.</h2>
-                        <div >
-                            <h4>Retour a la Page <a href="index.html"> Acceuil=></a></h4>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </body>
-    </html>
-<?php>
-';
+echo" Envoyer avec succes. Merci a vous.";
 
 
 >
